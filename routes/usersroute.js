0435504154ded524f4b63b4dafc1dcbe895875eb
Blue Pigeons <onlysky@hotmail.co.uk>
2016-09-09 09:55:23 +0100

    seperating the basic polyanno framework from the Edinburgh specific website and also trying to identify why the LUNA images are not loading which is possibly because of where I am using WiFi though..

--- routes/usersroute.js
+++ routes/usersroute.js
@@ -3,64 +3,13 @@
 
 var express    = require('express');
 var bodyParser = require('body-parser');
-
-var newVector     = require('./newVector');
-var newTranslate    = require('./newTranslation');
-var newTranscription    = require('./newTranscription');
-var newUser = require('./newUser');
-
-var websiteAddress = "http://localhost:8080";
-
-var vectorURL = websiteAddress.concat("/api/vectors/");
-var transcriptionURL = websiteAddress.concat("/api/transcriptions/");
-var translationURL = websiteAddress.concat("/api/translations/");
-
-var rejectionOptions = new Set(["false",'""' , null , false , 'undefined']);
-
-//ROUTE FUNCTIONS
-
-var isUseless = function(something) {
-  if (rejectionOptions.has(something) || rejectionOptions.has(typeof something)) {  return true;  }
-  else {  return false;  };
-};
-
-var asyncPush = function(addArray, oldArray) {
-    var theArray = oldArray;
-    var mergedArray = function() {
-        addArray.forEach(function(addDoc){
-            theArray.push(addDoc);
-        });
-        if (theArray.length = (oldArray.length + addArray.length)) {
-            return theArray;
-        };
-    };
-    return mergedArray();
-};
-
-var fieldMatching = function(searchArray, field, fieldValue) {
-  if (isUseless(searchArray) || isUseless(field) || isUseless(fieldValue)) {  return false  }
-  else {
-    var theMatch = false; 
-    searchArray.forEach(function(childDoc){
-      if (childDoc[field] == fieldValue) {
-          theMatch = childDoc;
-      };
-    });
-    return theMatch;
-  };
-};
-
-var jsonFieldEqual = function(docField, bodyDoc, bodyField) {
-    if (isUseless(bodyDoc[bodyField]) == false ) {    return bodyDoc[bodyField];    }
-    else {    return docField;    };
-};
-
+var polyanno = require('./routes_setup');
 
 ////EXPORTED FUNCTIONS
 
 ///is this appropriate??
 exports.findAll = function(req, res) {
-    newUser.find(function(err, allusers) {
+    polyanno.user_model.find(function(err, allusers) {
         if (err) {res.send(err)}
         else { res.json(allusers); };
     });
@@ -69,7 +18,7 @@ exports.findAll = function(req, res) {
 ///just for dev purposes, probably don't want to deploy with unless caching
 exports.deleteAll = function(req, res) {
       
-    newUserfind(function(err, allusers) {
+    polyanno.user_modelfind(function(err, allusers) {
         if (err) {res.send(err)};
 
         allusers.forEach(function(theuser){
@@ -86,7 +35,7 @@ exports.deleteAll = function(req, res) {
 
 exports.addNew = function(req, res) {
     
-    var theuser = new newUser(); 
+    var theuser = new polyanno.user_model(); 
 
     theuser.username = req.body.username;
 
@@ -103,14 +52,14 @@ exports.addNew = function(req, res) {
 };
 
 exports.getByID = function(req, res) {
-    newUser.findById(req.params.user_id, function(err, theuser) {
+    polyanno.user_model.findById(req.params.user_id, function(err, theuser) {
         if (err) {res.send(err) }
         else { res.json(theuser) };  
     });
 };
 
 exports.getByUsername = function(req, res) {
-    newUser.findOne({ "username" : req.params.username }, function(err, theuser) { /////
+    polyanno.user_model.findOne({ "username" : req.params.username }, function(err, theuser) { /////
         if (err) {res.send(err) }
         else { res.json(theuser) };  
     });
@@ -120,35 +69,35 @@ exports.getByUsername = function(req, res) {
 ////unlike annos, the users are expecting to be updated one piece of info at a time - no arrays
 exports.updateOne = function(req, res) {
 
-    var updateDoc = newUser.findOne({ "username" : req.params.username }); ///////
+    var updateDoc = polyanno.user_model.findOne({ "username" : req.params.username }); ///////
     updateDoc.exec(function(err, theuser) {
 
         if (err) {res.send(err)};
 
         var updateDocs = function(annoType, editType) {
-        	if ( !isUseless(req.body.docs_edited[annoType][editType]) ) { 
+        	if ( !polyanno.isUseless(req.body.docs_edited[annoType][editType]) ) { 
         		theuser.docs_edited[annoType][editType].addToSet(req.body.docs_edited[annoType][editType]);
         	};
         };
 
         var updateAllEdits = function(annoType) {
-        	if (!isUseless(req.body.docs_edited[annoType])) {
+        	if (!polyanno.isUseless(req.body.docs_edited[annoType])) {
 	        	updateDocs(annoType, "created");
 	        	updateDocs(annoType, "edited");
 	        	updateDocs(annoType, "deleted");
         	};
         };
 
-        if (!isUseless(req.body.docs_edited)) {
+        if (!polyanno.isUseless(req.body.docs_edited)) {
         	updateAllEdits("vectors");
         	updateAllEdits("transcriptions");
         	updateAllEdits("translations");
         };
 
         var updateFavourite = function(theFavourite) {
-        	if (!isUseless(req.body.favourites.the_image)) { theFavourite.the_image = req.body.favourites.the_image };
-        	if (!isUseless(req.body.favourites.translations)) { theFavourite.translations = [req.body.favourites.translations] };
-        	if (!isUseless(req.body.favourites.transcriptions)) { theFavourite.transcriptions = [req.body.favourites.transcriptions] };
+        	if (!polyanno.isUseless(req.body.favourites.the_image)) { theFavourite.the_image = req.body.favourites.the_image };
+        	if (!polyanno.isUseless(req.body.favourites.translations)) { theFavourite.translations = [req.body.favourites.translations] };
+        	if (!polyanno.isUseless(req.body.favourites.transcriptions)) { theFavourite.transcriptions = [req.body.favourites.transcriptions] };
         	return theFavourite;
         };
 
@@ -158,7 +107,7 @@ exports.updateOne = function(req, res) {
         	theuser.favourites.addToSet(newFavourite);
         };
 
-        if (!isUseless(req.body.favourites)) {
+        if (!polyanno.isUseless(req.body.favourites)) {
         	var existingFav = false;
         	theuser.favourites.forEach(function(userFavourite) {
         		if (userFavourite.image_id == req.body.favourites.image_id) {
@@ -169,14 +118,14 @@ exports.updateOne = function(req, res) {
         	if (existingFav == false) {	createNewFavourite(); };
         };
 
-        if (!isUseless(req.body.removefavourite)) {
+        if (!polyanno.isUseless(req.body.removefavourite)) {
         	var thefavourite;
         	theuser.favourites.forEach(function(userFavourite) {
         		if (theuser.favourites.image_id == req.body.removefavourite.image_id) {	thefavourite = userFavourite;	};
         	});
-        	if (!isUseless(req.body.removefavourite.the_image)) { thefavourite.the_image = false; };
-        	if (!isUseless(req.body.removefavourite.transcriptions)) { thefavourite.transcriptions.pull(req.body.removefavourite.transcriptions); };
-        	if (!isUseless(req.body.removefavourite.translations)) { thefavourite.translations.pull(req.body.removefavourite.translations); };
+        	if (!polyanno.isUseless(req.body.removefavourite.the_image)) { thefavourite.the_image = false; };
+        	if (!polyanno.isUseless(req.body.removefavourite.transcriptions)) { thefavourite.transcriptions.pull(req.body.removefavourite.transcriptions); };
+        	if (!polyanno.isUseless(req.body.removefavourite.translations)) { thefavourite.translations.pull(req.body.removefavourite.translations); };
         };
 
         theuser.save(function(err) {
@@ -189,7 +138,7 @@ exports.updateOne = function(req, res) {
 
 /////necessary?
 exports.deleteOne = function(req, res) {
-    newUser.remove({
+    polyanno.user_model.remove({
         _id: req.body.user_id
     }, 
     function(err, theuser) {
--- server.js
+++ server.js
@@ -1,20 +1,15 @@
 
 // SETUP REQUIREMENTS
 
-var databaseURL = 'mongodb://localhost:27017/testMongoDB';
-var thisWebsitePort = 8080;
-
 var express    = require('express');
 var bodyParser = require('body-parser');
 var mongoose   = require('mongoose');
 var cookieParser = require('cookie-parser');
 var cors = require('cors');
 
-var annotations = require('./routes/annotations');
-var vectors = require('./routes/vectors');
-var transcriptions = require('./routes/transcriptions');
-var translations = require('./routes/translations');
-var users = require('./routes/usersroute');
+var polyanno = require('polyanno_storage');
+var databaseURL = polyanno.setup.databaseport;
+var thisWebsitePort = polyanno.setup.applicationport;
 
 // GET APPLICATION RUNNING
 
@@ -103,61 +98,61 @@ app.get('/aboutus', function(req, res) {
 
 /////////////////USER ROUTES
 
-userRouter.get('/username/:username', users.getByUsername);
-userRouter.get('/id/:user_id', users.getByID);
-userRouter.post('/', users.addNew);
-userRouter.put('/:username', users.updateOne);
+userRouter.get('/username/:username', polyanno.users.getByUsername);
+userRouter.get('/id/:user_id', polyanno.users.getByID);
+userRouter.post('/', polyanno.users.addNew);
+userRouter.put('/:username', polyanno.users.updateOne);
 
 /////////////////API ROUTES
 
 //ANNOTATIONS API
 
-annoRouter.post('/annotations', annotations.addNew );
-annoRouter.put( annotations.updateOne ); ///
-annoRouter.get('/annotations', annotations.getAll);
-annoRouter.get('/annotations/vectors', annotations.getAllVectorAnnos);
-annoRouter.get('/annotations/transcriptions', annotations.getAllTranscriptionAnnos);
-annoRouter.get('/annotations/translations', annotations.getAllTranslationAnnos);
-annoRouter.get('/annotations/:anno_id', annotations.getByID);
-annoRouter.get('/annotations/target/:target_id', annotations.getByTarget);
+annoRouter.post('/annotations', polyanno.annotations.addNew );
+annoRouter.put( polyanno.annotations.updateOne ); ///
+annoRouter.get('/annotations', polyanno.annotations.getAll);
+annoRouter.get('/annotations/vectors', polyanno.annotations.getAllVectorAnnos);
+annoRouter.get('/annotations/transcriptions', polyanno.annotations.getAllTranscriptionAnnos);
+annoRouter.get('/annotations/translations', polyanno.annotations.getAllTranslationAnnos);
+annoRouter.get('/annotations/:anno_id', polyanno.annotations.getByID);
+annoRouter.get('/annotations/target/:target_id', polyanno.annotations.getByTarget);
 
 //VECTOR API
 
-annoRouter.get('/vectors', vectors.findAll);
-annoRouter.post('/vectors', vectors.addNew);
-annoRouter.get('/vectors/:vector_id', vectors.getByID);
-annoRouter.put('/vectors/:vector_id', vectors.updateOne);
-annoRouter.delete('/vectors/:vector_id', vectors.deleteOne);
-annoRouter.delete('/vectors', vectors.deleteAll);
+annoRouter.get('/vectors', polyanno.vectors.findAll);
+annoRouter.post('/vectors', polyanno.vectors.addNew);
+annoRouter.get('/vectors/:vector_id', polyanno.vectors.getByID);
+annoRouter.put('/vectors/:vector_id', polyanno.vectors.updateOne);
+annoRouter.delete('/vectors/:vector_id', polyanno.vectors.deleteOne);
+annoRouter.delete('/vectors', polyanno.vectors.deleteAll);
 
-annoRouter.get('/vectors/targets/:target', annotations.getVectorsByTarget);
-annoRouter.get('/vectors/ids/:_ids', vectors.searchByIds);
+annoRouter.get('/vectors/targets/:target', polyanno.annotations.getVectorsByTarget);
+annoRouter.get('/vectors/ids/:_ids', polyanno.vectors.searchByIds);
 
 //TRANSCRIPTION API
 
-annoRouter.get('/transcriptions', transcriptions.findAll);
-annoRouter.post('/transcriptions', transcriptions.addNew);
-annoRouter.get('/transcriptions/:transcription_id', transcriptions.getByID);
-annoRouter.put('/transcriptions/:transcription_id', transcriptions.updateOne);
-annoRouter.put('/transcriptions/voting/:voteType', transcriptions.voting);
-annoRouter.delete('/transcriptions/:transcription_id', transcriptions.deleteOne);
-annoRouter.delete('/transcriptions', transcriptions.deleteAll);
+annoRouter.get('/transcriptions', polyanno.transcriptions.findAll);
+annoRouter.post('/transcriptions', polyanno.transcriptions.addNew);
+annoRouter.get('/transcriptions/:transcription_id', polyanno.transcriptions.getByID);
+annoRouter.put('/transcriptions/:transcription_id', polyanno.transcriptions.updateOne);
+annoRouter.put('/transcriptions/voting/:voteType', polyanno.transcriptions.voting);
+annoRouter.delete('/transcriptions/:transcription_id', polyanno.transcriptions.deleteOne);
+annoRouter.delete('/transcriptions', polyanno.transcriptions.deleteAll);
 
-annoRouter.get('/transcriptions/targets/:target', annotations.getTranscriptionsByTarget);
-annoRouter.get('/transcriptions/ids/:_ids', transcriptions.searchByIds);
+annoRouter.get('/transcriptions/targets/:target', polyanno.annotations.getTranscriptionsByTarget);
+annoRouter.get('/transcriptions/ids/:_ids', polyanno.transcriptions.searchByIds);
 
 //TRANSLATION API
 
-annoRouter.get('/translations', translations.findAll);
-annoRouter.post('/translations', translations.addNew);
-annoRouter.get('/translations/:transcription_id', translations.getByID);
-annoRouter.put('/translations/:transcription_id', translations.updateOne);
-annoRouter.put('/translations/voting/:voteType', translations.voting);
-annoRouter.delete('/translations/:transcription_id', translations.deleteOne);
-annoRouter.delete('/translations', translations.deleteAll);
+annoRouter.get('/translations', polyanno.translations.findAll);
+annoRouter.post('/translations', polyanno.translations.addNew);
+annoRouter.get('/translations/:transcription_id', polyanno.translations.getByID);
+annoRouter.put('/translations/:transcription_id', polyanno.translations.updateOne);
+annoRouter.put('/translations/voting/:voteType', polyanno.translations.voting);
+annoRouter.delete('/translations/:transcription_id', polyanno.translations.deleteOne);
+annoRouter.delete('/translations', polyanno.translations.deleteAll);
 
-annoRouter.get('/translations/targets/:target', annotations.getTranslationsByTarget);
-annoRouter.get('/translations/ids/:_ids', translations.searchByIds);
+annoRouter.get('/translations/targets/:target', polyanno.annotations.getTranslationsByTarget);
+annoRouter.get('/translations/ids/:_ids', polyanno.translations.searchByIds);
 
 /////////GET STARTED
 
@@ -171,6 +166,4 @@ app.use(function(req, res, next) {
 
 //tells the server where it should be listening for req and res
 app.listen(thisWebsitePort);
-
-//will need to configure stuff for actual deployment
 
